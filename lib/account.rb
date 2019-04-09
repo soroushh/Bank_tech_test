dee81d683dbac380971f1db65c31dc85e72638fd
Soroush Khosravi <sorush.kh68@gmail.com>
2019-04-09 12:00:22 +0100

    We can add money to the account by add_deposite method

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,11 @@
 class Account
+  attr_reader :balance
+  def initialize(balance=0)
+    @balance = balance
+  end
 
-end 
+  def add_deposite(money_ammount)
+    @balance += money_ammount
+  end
+
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,4 +3,12 @@ describe Account do
   it "we are able to make an account" do
     account = Account.new()
   end
+
+  describe "#add_deposite" do
+    it " When we add dopiste, the balance of account gets more" do
+      account = Account.new()
+      account.add_deposite(500)
+      expect(account.balance).to eq 500
+    end
+  end
 end
